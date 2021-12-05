463124ae90e7b33cbec82dc694a5412bea8d9a12
YetiBarBar <yetibarbar@gmail.com>
2021-12-05 21:31:59 +0100

    Add: Add solution for day 6

--- src/day5.rs
+++ src/day5.rs
@@ -2,22 +2,30 @@ use std::collections::HashSet;
 
 use adventofcode_2020::utils::read_lines;
 
+#[must_use]
 pub fn part_1(data: &[String]) -> usize {
-    data.iter().map(|seat| decode_part_1(seat)).max().unwrap()
+    data.iter()
+        .map(|seat| decode_part_1(seat))
+        .max()
+        .unwrap_or_default()
 }
 
+#[must_use]
 pub fn decode_part_1(data: &str) -> usize {
     decode_seat(&data[0..7]) * 8 + decode_row(&data[7..])
 }
 
+#[must_use]
 pub fn decode_seat(seat: &str) -> usize {
     decode(seat, &[64, 32, 16, 8, 4, 2, 1], 'B')
 }
 
+#[must_use]
 pub fn decode_row(row: &str) -> usize {
     decode(row, &[4, 2, 1], 'R')
 }
 
+#[must_use]
 pub fn decode(in_data: &str, pows: &[usize], symbol: char) -> usize {
     pows.iter()
         .zip(in_data.chars())
@@ -28,6 +36,7 @@ pub fn decode(in_data: &str, pows: &[usize], symbol: char) -> usize {
         .sum()
 }
 
+#[must_use]
 pub fn part_2(data: &[String]) -> usize {
     let seats: HashSet<_> = data.iter().map(|seat| decode_part_1(seat)).collect();
 
@@ -37,11 +46,11 @@ pub fn part_2(data: &[String]) -> usize {
     // So our Id is between 9 to (127 * 8 + 1)
     for idx in 9..(127 * 8) {
         if !seats.contains(&idx) && seats.contains(&(idx - 1)) && seats.contains(&(idx + 1)) {
-            candidates.push(idx)
+            candidates.push(idx);
         }
     }
 
-    *candidates.first().unwrap()
+    *candidates.first().unwrap_or(&0)
 }
 
 pub fn main() {
---
+++ src/day6.rs
@@ -0,0 +1,98 @@
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
+
+#[must_use]
+pub fn part_1(data: &[String]) -> usize {
+    data.iter()
+        .map(|s| {
+            s.trim()
+                .chars()
+                .filter(|&c| c != '\n')
+                .collect::<HashSet<char>>()
+                .len()
+        })
+        .sum()
+}
+
+#[must_use]
+pub fn part_2(data: &[String]) -> usize {
+    data.iter()
+        .map(|group| {
+            let mut hmap = HashMap::<char, usize>::new();
+            for line in group.lines() {
+                let chars = line.chars().collect::<HashSet<char>>();
+                for c in &chars {
+                    *hmap.entry(*c).or_insert(0) += 1;
+                }
+            }
+            hmap.iter()
+                .filter(|(_, count)| **count == group.lines().count())
+                .count()
+        })
+        .sum()
+}
+
+pub fn main() {
+    // Read file to a single string
+    let mut filepath: PathBuf = std::env::current_dir().unwrap();
+    filepath.push("data");
+    filepath.push("day_2020_6.data");
+    let input_data = std::fs::read_to_string(filepath).unwrap();
+
+    let groups: Vec<String> = input_data
+        .split("\n\n")
+        .map(|s| str::to_string(s))
+        .collect();
+
+    println!("Part 1: {:?}", part_1(&groups));
+    println!("Part 2: {:?}", part_2(&groups));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_groups_part_1() {
+        let input = r#"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"#;
+        let groups: Vec<String> = input.split("\n\n").map(|s| str::to_string(s)).collect();
+        assert_eq!(part_1(&groups), 11);
+    }
+
+    #[test]
+    fn test_groups_part_2() {
+        let input = r#"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"#;
+        let groups: Vec<String> = input.split("\n\n").map(|s| str::to_string(s)).collect();
+        assert_eq!(part_2(&groups), 6);
+    }
+}
